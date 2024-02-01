bb96d9ddc08493ad965aa2c3060ea08378523006
AWEEE0413 <jordan2262505@gmail.com>
2024-02-01 11:29:45 +0800

    X

--- PLAYER.py
+++ PLAYER.py
@@ -1,24 +1,34 @@
 import os
 import keyboard
+import pygame  # 需要先安裝 pygame 庫，可以使用 pip install pygame 來安裝
 
 def play_next_song(folder_path):
     """
     Plays the next song in the folder.
     """
-    # 在播放下一首歌曲之前顯示歌名
-    display_song_name()
-    
-    # 播放下一首歌曲的程式碼
     song_list = os.listdir(folder_path)
-    # Add your logic to play the next song from the song_list
-    
-def display_song_name():
+    if not song_list:
+        print("No songs found in the folder.")
+        return
+
+    # 選擇第一首歌曲來播放
+    song_path = os.path.join(folder_path, song_list[0])
+
+    # 在播放下一首歌曲之前顯示歌名
+    display_song_name(song_path)
+
+    # 播放下一首歌曲的程式碼
+    pygame.mixer.init()
+    pygame.mixer.music.load(song_path)
+    pygame.mixer.music.play()
+
+def display_song_name(song_path):
     """
     Displays the name of the current song.
     """
-    # 顯示歌名的程式碼
-    pass
+    song_name = os.path.basename(song_path)
+    print("Now playing: ", song_name)
 
-folder_path = r"C:\Users\kung\樹洞\PY"  # Replace with the actual folder path
-keyboard.add_hotkey('c', lambda: (play_next_song(folder_path), display_song_name()))
+folder_path = "C:\\Users\\kung\\樹洞\\PY"  # Replace with the actual folder path
+keyboard.add_hotkey('c', lambda: play_next_song(folder_path))
 keyboard.wait('esc')
